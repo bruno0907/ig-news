f7f2770953704bab0340692006b35b7dfba567bb
bruno0907@gmail.com <bruno0907@gmail.com>
2021-05-21 22:02:25 -0300

    feat: added user creation method to faunadb from github oAuth user

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
+import { fauna } from '../../../services/fauna'
+import { query } from 'faunadb'
+
 export default NextAuth({
   providers: [
     Providers.GitHub({
@@ -8,5 +11,30 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET, // Info gerada NO PERFIL do github
       scope: 'read:user' // O oAuth do github precisa que o escopo da auth seja definida. Ver docs. Valores separados por vírgula
     })
-  ]
+  ],
+
+  jwt: {
+    signingKey: process.env.SIGNIN_KEY,
+  },
+
+  callbacks: {    
+    async signIn(user, account, profile) {
+      const { email } = user
+
+      try {       
+        await fauna.query(
+          query.Create(
+            query.Collection('users'),
+            { data: { email }}
+          )
+        )
+
+        return true        
+      } catch {
+        return false
+
+      }
+    },
+
+  }  
 })
