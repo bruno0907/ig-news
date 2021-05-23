5e83c5502e187de41435ee1af7423d71dc5ee3e9
bruno0907@gmail.com <bruno0907@gmail.com>
2021-05-23 11:32:40 -0300

    feat: added user verification existance to nextAuth method to prevent double register creation and oAuth authentication errors

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,7 +2,7 @@ import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
 import { fauna } from '../../../services/fauna'
-import { query } from 'faunadb'
+import { query as q } from 'faunadb'
 
 export default NextAuth({
   providers: [
@@ -13,19 +13,31 @@ export default NextAuth({
     })
   ],
 
-  jwt: {
-    signingKey: process.env.SIGNIN_KEY,
-  },
-
   callbacks: {    
     async signIn(user, account, profile) {
       const { email } = user
 
       try {       
         await fauna.query(
-          query.Create(
-            query.Collection('users'),
-            { data: { email }}
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: email }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
           )
         )
 
