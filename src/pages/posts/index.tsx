8aac68217bbe86836ce099bf64f02b0f7e905e19
bruno0907@gmail.com <bruno0907@gmail.com>
2021-05-25 18:06:48 -0300

    chore: fixed ActiveLink component style error. Added a post page and its styles. changed posts styles file name. added a session validator do nex-auth callback

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,10 +1,12 @@
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
+
 import { getPrismicCliente } from '../../services/prismic'
 import Prismic from '@prismicio/client'
 import { RichText } from 'prismic-dom'
 
-import styles from './styles.module.scss'
+import styles from './posts.module.scss'
 
 type Post = {
   slug: string;
@@ -26,11 +28,13 @@ export default function Posts({ posts }: PostsProps){
       <main className={styles.container}>
         <div className={styles.posts}>   
           { posts.map(post => (
-            <a key={post.slug} href={post.slug}>
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}  
         </div>
       </main>
